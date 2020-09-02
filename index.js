1697bd40b88d218633765bf6230a23625bebf691
raiders032 <nameks@naver.com>
2020-09-02 22:33:32 +0900

    2.6 Express Core: Middlewares

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const PORT = 3000;
 const handleListening = () =>
   console.log(`listening at http://localhost:${PORT}`);
 
-const handleHome = (req, res) => res.send("Hello world!!");
+const handleHome = (req, res) => res.send("Hello world!!!");
 
 const handleProfile = (req, res) => res.send("you are on my profile");
+
+const betweenHome = (req, res, next) => {
+  console.log("between");
+  next();
+};
+
+app.use(betweenHome);
 
 app.get("/", handleHome);
 
