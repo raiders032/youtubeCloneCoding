f247441c269dc2ae045e8a000533cbf2d5fc8511
raiders032 <nameks@naver.com>
2020-09-02 23:02:12 +0900

    2.7 Express Core: Middlewares part Two

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 const PORT = 3000;
 
@@ -14,6 +19,11 @@ const betweenHome = (req, res, next) => {
   next();
 };
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(morgan("dev"));
 app.use(betweenHome);
 
 app.get("/", handleHome);
