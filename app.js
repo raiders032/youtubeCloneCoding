81db4bf10fc726189562efaf991d828d8c7eb884
raiders032 <nameks@naver.com>
2020-09-03 21:46:55 +0900

    2.9 MVC Pattern part One

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
@@ -13,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
 
+app.use("/", globalRouter);
+app.use("/video", videoRouter);
 app.use("/user", userRouter);
 
 export default app;
--- router.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-
-export const userRouter = express.Router();
-
-userRouter.get("/", (req, res) => res.send("hello world!!!"));
-userRouter.get("/profile", (req, res) => res.send("user profile!!!"));
-userRouter.get("/edit", (req, res) => res.send("user edit!!!"));
