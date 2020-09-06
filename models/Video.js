95caf1eada89cbf4a296c7904856057fc105127d
raiders032 <nameks@naver.com>
2020-09-06 20:18:19 +0900

    3.3 Video Model

---
+++ models/Video.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+  fileUrl: {
+    type: String,
+    required: "Filr url is required",
+  },
+  view: {
+    type: Number,
+    default: 0,
+  },
+  title: {
+    type: String,
+    required: "title is requierd",
+  },
+  description: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const model = mongoose.model("Video", VideoSchema);
+export default model;
